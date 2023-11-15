e6af0368ae2167262eb3334c19da54f687f8be46
Leon <kellerlukas03@gmail.com>
2023-11-15 09:19:25 +0100

    bugs

--- EndlessRunner/Assets/Scripts/New Ground.cs
+++ EndlessRunner/Assets/Scripts/New Ground.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class NewGround : MonoBehaviour
 {
+    public GameObject newGround;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class NewGround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                Instantiate(newGround,transform.position,Quaternion.identity);
+                print("LoL");
+            }
+        }
     }
 }
