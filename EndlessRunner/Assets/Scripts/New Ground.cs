466a26f147e34a24edaefe7edbdcf3e85412d251
leontec06 <leontec06@gmail.com>
2023-11-17 11:55:47 +0100

    water

--- EndlessRunner/Assets/Scripts/New Ground.cs
+++ EndlessRunner/Assets/Scripts/New Ground.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class NewGround : MonoBehaviour
 {
     public GameObject newGround;
+    public GameObject water;
     private int levelnumber=1;
     // Start is called before the first frame update
 
@@ -29,12 +30,10 @@ public class NewGround : MonoBehaviour
             
             if (other.gameObject.tag == "Player")
             {
-                Instantiate(newGround, new Vector3(0f,0f,60f*levelnumber+120), Quaternion.identity);
-
-
-                
+                Instantiate(newGround, new Vector3(0f,0f,60f*levelnumber+120), Quaternion.identity);                
                 transform.Translate(new Vector3(0f,0f,60f));
 
+                Instantiate(water, new Vector3(0f,-10f,60f*levelnumber+120), Quaternion.identity); 
                 
                 levelnumber+=1;
                 print(levelnumber);
